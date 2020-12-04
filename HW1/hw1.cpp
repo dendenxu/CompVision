98731f6f8aba5ee499a8707072b00f30c0e1fd9e
dendenxu <56295612@qq.com>
2020-12-04 17:01:14 +0800

    imshow test

--- HW1/hw1.cpp
+++ HW1/hw1.cpp
@@ -41,6 +41,9 @@ int imageFilter(int argc, char* argv[]);
 void sharpen(const Mat& img, Mat& result);
 static void imageFilterHelp(char* progName);
 
+int imageShow(int argc, char* argv[]);
+
+
 void VideoIOHelpMessage()
 {
     cout
@@ -68,15 +71,48 @@ void VideoCompareHelpMessage()
 
 int main(int argc, char* argv[])
 {
-    return imageFilter(argc, argv);
-    //return VideoIO(argc, argv);
-    //return VideoCompare(argc, argv);
-    //DrawCircles();
-    //DrawAtomAndRook();
+    return imageShow(argc, argv);
+    //return imageFilter(argc, argv);
+    // return VideoIO(argc, argv);
+    // return VideoCompare(argc, argv);
+    // DrawCircles();
+    // DrawAtomAndRook();
     // waitKey(0);
 }
 
-static void imageFilterHelp(char* progName)
+int imageShow(int argc, char* argv[])
+{
+    if (argc < 2) {
+        OUTPUTERROR << "Not enough parameters" << endl;
+    }
+    string imgName = argv[1];
+    Mat img = imread(samples::findFile(imgName));
+    if (img.empty()) {
+        OUTPUTERROR << "Cannot find image file: " << imgName << endl;
+    }
+
+    Mat grey;
+    cvtColor(img, grey, COLOR_BGR2GRAY);
+    Mat sobel;
+    Sobel(grey, sobel, CV_32F, 1, 0);
+    double minVal, maxVal;
+    minMaxLoc(sobel, &minVal, &maxVal);
+    Mat draw;
+    double scale = 255.0 / (maxVal - minVal), delta = -minVal * scale;
+    sobel.convertTo(draw, CV_8U, scale, delta);
+    string imgNameNoExt = imgName.substr(0, imgName.find_last_of('.'));
+    string WIN_GREY = "Original Grey: " + imgNameNoExt;
+    string WIN_SOBELGREY = "SOBEL Grey: " + imgNameNoExt;
+    namedWindow(WIN_GREY, WINDOW_AUTOSIZE);
+    namedWindow(WIN_SOBELGREY, WINDOW_AUTOSIZE);
+    moveWindow(WIN_GREY, 400, 100);
+    moveWindow(WIN_SOBELGREY, grey.cols + 400, 100);
+    imshow(WIN_GREY, grey);
+    imshow(WIN_SOBELGREY, draw);
+    waitKey();
+}
+
+void imageFilterHelp(char* progName)
 {
     cout << endl
         << "This program shows how to filter images with mask: the write it yourself and the"
@@ -132,8 +168,7 @@ void sharpen(const Mat& img, Mat& result)
         for (int i = nChannels; i < nChannels * (img.cols - 1); ++i) {
             *output++ =
                 // ! Using saturate_cast to clip to bounds
-                saturate_cast<uchar>
-                (5 * current[i] - current[i - nChannels] - current[i + nChannels] - previous[i] - next[i]);
+                saturate_cast<uchar>(5 * current[i] - current[i - nChannels] - current[i + nChannels] - previous[i] - next[i]);
         }
     }
     result.row(0).setTo(Scalar(0));
