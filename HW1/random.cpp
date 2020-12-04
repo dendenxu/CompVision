e9fc4c50e2ffc9d18f87720ba5c81f91d8cd12e7
dendenxu <56295612@qq.com>
2020-12-04 23:00:33 +0800

    rescaling

--- HW1/random.cpp
+++ HW1/random.cpp
@@ -1,18 +1,29 @@
 #include "include.hpp"
+#define CORRECTION(x) (static_cast<int>((static_cast<double>(x)) / 900 * WINDOW_WIDTH))
 
-constexpr auto NUMBER = 100;
-constexpr auto DELAY = 5; // ! in ms
-constexpr auto WINDOW_WIDTH = 900;
-constexpr auto WINDOW_HEIGHT = 600;
-constexpr auto WIN_W_MIN = -WINDOW_WIDTH / 2;
-constexpr auto WIN_W_MAX = 3 * WINDOW_WIDTH / 2;
-constexpr auto WIN_H_MIN = -WINDOW_HEIGHT / 2;
-constexpr auto WIN_H_MAX = 3 * WINDOW_HEIGHT / 2;
+constexpr auto repeatCount = 100;
+constexpr auto frameTime = 5;  // ! in ms
+constexpr auto WINDOW_WIDTH = 1920;
+constexpr auto WINDOW_HEIGHT = 1080;
+constexpr auto xMin = -WINDOW_WIDTH / 2;
+constexpr auto xMax = 3 * WINDOW_WIDTH / 2;
+constexpr auto yMin = -WINDOW_HEIGHT / 2;
+constexpr auto yMax = 3 * WINDOW_HEIGHT / 2;
 
-constexpr auto ELLI_AXES_MIN = 0;
-constexpr auto ELLI_AXES_MAX = 200;
-constexpr auto ELLI_ANGLE_MIN = 0;
-constexpr auto ELLI_ANGLE_MAX = 180;
+constexpr auto axesMin = 0;
+constexpr auto axesMax = CORRECTION(200);
+constexpr auto angleMin = 0;
+constexpr auto angleMax = CORRECTION(180);
+
+constexpr auto lineWidthMax = CORRECTION(10);
+constexpr auto recLineWidthMax = CORRECTION(10);
+constexpr auto elliLineWidthMax = CORRECTION(9);
+
+constexpr auto fontScaleMin = 0 + 0.1;
+constexpr auto fontScaleMax = CORRECTION(5) + 0.1;
+constexpr auto fontThickness = CORRECTION(5);
+constexpr auto headFontThickness = CORRECTION(5);
+constexpr auto headFontScale = CORRECTION(3) + 0.0;
 
 int imageRandom(int argc, char* argv[])
 {
@@ -22,7 +33,7 @@ int imageRandom(int argc, char* argv[])
     Mat image = Mat::zeros(WINDOW_HEIGHT, WINDOW_WIDTH, CV_8UC3);
     imshow(window_name, image);
     //setWindowProperty(WIN_RANDOM, WND_PROP_FULLSCREEN, WINDOW_FULLSCREEN);
-    waitKey(DELAY);
+    waitKey(frameTime);
 
     drawRandomLines(image, window_name, rng);
     drawRandomRectangles(image, window_name, rng);
@@ -48,15 +59,15 @@ int drawRandomLines(Mat image, const string window_name, RNG rng)
 {
     Point pt1, pt2;
 
-    for (int i = 0; i < NUMBER; i++) {
-        pt1.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt1.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt2.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt2.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
+    for (int i = 0; i < repeatCount; i++) {
+        pt1.x = rng.uniform(xMin, xMax);
+        pt1.y = rng.uniform(yMin, yMax);
+        pt2.x = rng.uniform(xMin, xMax);
+        pt2.y = rng.uniform(yMin, yMax);
 
-        line(image, pt1, pt2, randomColor(rng), rng.uniform(1, 10), LINE_AA);
+        line(image, pt1, pt2, randomColor(rng), rng.uniform(1, lineWidthMax), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
@@ -68,15 +79,15 @@ int drawRandomRectangles(Mat image, const string window_name, RNG rng)
 {
     Point pt1, pt2;
 
-    for (int i = 0; i < NUMBER; i++) {
-        pt1.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt1.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt2.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt2.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
+    for (int i = 0; i < repeatCount; i++) {
+        pt1.x = rng.uniform(xMin, xMax);
+        pt1.y = rng.uniform(yMin, yMax);
+        pt2.x = rng.uniform(xMin, xMax);
+        pt2.y = rng.uniform(yMin, yMax);
 
-        rectangle(image, pt1, pt2, randomColor(rng), MAX(rng.uniform(-3, 10), -1), LINE_AA);
+        rectangle(image, pt1, pt2, randomColor(rng), MAX(rng.uniform(-3, recLineWidthMax), -1), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
@@ -90,16 +101,18 @@ int drawRandomEllipses(Mat image, const string window_name, RNG rng)
     Point center;
     Size axes;
     double angle;
-    for (int i = 0; i < NUMBER; i++) {
-        center.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        center.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        axes.width = rng.uniform(ELLI_AXES_MIN, ELLI_AXES_MAX);
-        axes.height = rng.uniform(ELLI_AXES_MIN, ELLI_AXES_MAX);
-        angle = rng.uniform(ELLI_ANGLE_MIN, ELLI_ANGLE_MAX);
+    for (int i = 0; i < repeatCount; i++) {
+        center.x = rng.uniform(xMin, xMax);
+        center.y = rng.uniform(yMin, yMax);
+        axes.width = rng.uniform(axesMin, axesMax);
+        axes.height = rng.uniform(axesMin, axesMax);
+        angle = rng.uniform(angleMin, angleMax);
 
-        ellipse(image, center, axes, angle, angle - 100, angle + 200, randomColor(rng), rng.uniform(-1, 9), LINE_AA);
+        OUTPUTINFO << "Getting axes" << axes << "for index: " << i << endl;
+
+        ellipse(image, center, axes, angle, angle - 100, angle + 200, randomColor(rng), rng.uniform(-1, elliLineWidthMax), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
@@ -112,22 +125,22 @@ int drawRandomPolylines(Mat image, const string window_name, RNG rng)
     Point pt[2][3];
     const Point* ppt[2] = {pt[0], pt[1]};
     int npt[] = {3, 3};
-    for (int i = 0; i < NUMBER; i++) {
-        pt[0][0].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[0][0].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[0][1].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[0][1].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[0][2].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[0][2].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[1][0].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[1][0].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[1][1].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[1][1].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[1][2].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[1][2].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        polylines(image, ppt, npt, 2, true, randomColor(rng), rng.uniform(1, 10), LINE_AA);
+    for (int i = 0; i < repeatCount; i++) {
+        pt[0][0].x = rng.uniform(xMin, xMax);
+        pt[0][0].y = rng.uniform(yMin, yMax);
+        pt[0][1].x = rng.uniform(xMin, xMax);
+        pt[0][1].y = rng.uniform(yMin, yMax);
+        pt[0][2].x = rng.uniform(xMin, xMax);
+        pt[0][2].y = rng.uniform(yMin, yMax);
+        pt[1][0].x = rng.uniform(xMin, xMax);
+        pt[1][0].y = rng.uniform(yMin, yMax);
+        pt[1][1].x = rng.uniform(xMin, xMax);
+        pt[1][1].y = rng.uniform(yMin, yMax);
+        pt[1][2].x = rng.uniform(xMin, xMax);
+        pt[1][2].y = rng.uniform(yMin, yMax);
+        polylines(image, ppt, npt, 2, true, randomColor(rng), rng.uniform(1, lineWidthMax), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
@@ -139,22 +152,22 @@ int drawRandomFilledPolygons(Mat image, const string window_name, RNG rng)
     Point pt[2][3];
     const Point* ppt[2] = {pt[0], pt[1]};
     int npt[] = {3, 3};
-    for (int i = 0; i < NUMBER; i++) {
-        pt[0][0].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[0][0].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[0][1].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[0][1].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[0][2].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[0][2].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[1][0].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[1][0].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[1][1].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[1][1].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        pt[1][2].x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        pt[1][2].y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
+    for (int i = 0; i < repeatCount; i++) {
+        pt[0][0].x = rng.uniform(xMin, xMax);
+        pt[0][0].y = rng.uniform(yMin, yMax);
+        pt[0][1].x = rng.uniform(xMin, xMax);
+        pt[0][1].y = rng.uniform(yMin, yMax);
+        pt[0][2].x = rng.uniform(xMin, xMax);
+        pt[0][2].y = rng.uniform(yMin, yMax);
+        pt[1][0].x = rng.uniform(xMin, xMax);
+        pt[1][0].y = rng.uniform(yMin, yMax);
+        pt[1][1].x = rng.uniform(xMin, xMax);
+        pt[1][1].y = rng.uniform(yMin, yMax);
+        pt[1][2].x = rng.uniform(xMin, xMax);
+        pt[1][2].y = rng.uniform(yMin, yMax);
         fillPoly(image, ppt, npt, 2, randomColor(rng), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
@@ -163,13 +176,13 @@ int drawRandomFilledPolygons(Mat image, const string window_name, RNG rng)
 }
 int drawRandomCircles(Mat image, const string window_name, RNG rng)
 {
-    for (int i = 0; i < NUMBER; i++) {
+    for (int i = 0; i < repeatCount; i++) {
         Point center;
-        center.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        center.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
-        circle(image, center, rng.uniform(0, 300), randomColor(rng), rng.uniform(-1, 9), LINE_AA);
+        center.x = rng.uniform(xMin, xMax);
+        center.y = rng.uniform(yMin, yMax);
+        circle(image, center, rng.uniform(0, 300), randomColor(rng), rng.uniform(-1, elliLineWidthMax), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
@@ -181,36 +194,37 @@ int drawRandomCircles(Mat image, const string window_name, RNG rng)
 int displayRandomText(Mat image, const string window_name, RNG rng)
 {
     Point org;
-    for (int i = 1; i < NUMBER; i++) {
-        org.x = rng.uniform(WIN_W_MIN, WIN_W_MAX);
-        org.y = rng.uniform(WIN_H_MIN, WIN_H_MAX);
+    for (int i = 1; i < repeatCount; i++) {
+        org.x = rng.uniform(xMin, xMax);
+        org.y = rng.uniform(yMin, yMax);
 
-        putText(image, __func__, org, rng.uniform(0, 8), rng.uniform(0.1, 5.1), randomColor(rng), rng.uniform(1, 10), LINE_AA);
+        putText(image, __func__, org, rng.uniform(0, 8), rng.uniform(fontScaleMin, fontScaleMax), randomColor(rng), rng.uniform(1, fontThickness), LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             OUTPUTERROR << "Something is wrong when drawing: " << dec << i << endl;
             return -1;
         }
     }
     return 0;
 }
+
 // TODO: Implement this
 int displayBigEnd(Mat image, const string window_name, RNG rng)
 {
-    Size textsize = getTextSize("Wakanda Forever", FONT_HERSHEY_SCRIPT_SIMPLEX, 3, 5, 0);
+    Size textsize = getTextSize("Wakanda Forever", FONT_HERSHEY_SCRIPT_SIMPLEX, headFontScale, headFontThickness, 0);
     Point org((WINDOW_WIDTH - textsize.width) / 2, (WINDOW_HEIGHT - textsize.height) / 2);
     Scalar startColor = randomColor(rng) / 10;
 
     int step = 2;
     for (int i = 0; i < 255; i += step) {
-        image -= Scalar::all(step*2); // ! The image should fade faster than the text
+        image -= Scalar::all(step * 2);  // ! The image should fade faster than the text
         for (auto& value : startColor.val) {
             value += step;
             value = saturate_cast<uchar>(value);
         }
-        putText(image, "Wakanda Forever", org, FONT_HERSHEY_SCRIPT_SIMPLEX, 3, startColor, 5, LINE_AA);
+        putText(image, "Wakanda Forever", org, FONT_HERSHEY_SCRIPT_SIMPLEX, headFontScale, startColor, headFontThickness, LINE_AA);
         imshow(window_name, image);
-        if (waitKey(DELAY) >= 0) {
+        if (waitKey(frameTime) >= 0) {
             return -1;
         }
     }
